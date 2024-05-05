75ebf331918b35bfd169137a384aae25a8fb1b41
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-05-05 20:52:29 +0530

    Initial commit

---
+++ src/main/java/com/example/shoe_shop_project/ShoeShopProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoe_shop_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeShopProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoeShopProjectApplication.class, args);
+    }
+
+}
