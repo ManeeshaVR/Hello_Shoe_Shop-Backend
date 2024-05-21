22cb19c39efdd15e055719e9086473f539689e38
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-05-21 20:50:11 +0530

    fix inventory controller

--- src/main/java/software/vimukthirajapaksha/shoe_shop_project/dao/ItemRepo.java
+++ src/main/java/software/vimukthirajapaksha/shoe_shop_project/dao/ItemRepo.java
@@ -1,9 +1,13 @@
 package software.vimukthirajapaksha.shoe_shop_project.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import software.vimukthirajapaksha.shoe_shop_project.entity.ItemEntity;
 
 @Repository
 public interface ItemRepo extends JpaRepository<ItemEntity, String> {
+    @Query("SELECT itemCode FROM ItemEntity  WHERE itemCode LIKE CONCAT(:prefix, '%') ORDER BY itemCode DESC LIMIT 1")
+    String findLastItemCodeWithPrefix(@Param("prefix") String prefix);
 }
