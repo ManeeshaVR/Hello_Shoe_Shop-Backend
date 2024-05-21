57ed1c67b26bb111766241134fa8f23adc2b825a
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-05-21 19:22:02 +0530

    add Exception

---
+++ src/main/java/software/vimukthirajapaksha/shoe_shop_project/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package software.vimukthirajapaksha.shoe_shop_project.exception;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message){
+        super(message);
+    }
+}
